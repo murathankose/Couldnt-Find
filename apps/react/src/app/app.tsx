bb18416057290494bc53109cf8697a515bbbca34
Evasionn <asalyazilim@gmail.com>
2020-10-08 12:44:12 +0300

    fixing router implementation to avoid recalling issue

--- apps/react/src/app/app.tsx
+++ apps/react/src/app/app.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import Routes from './Routes';
+
 import { Navigation } from '@internship/ui';
 import { Provider } from 'react-redux';
 import { configureStore } from '@internship/config';
-import { withRouter } from 'react-router-dom';
+import { Routes } from './Routes';
 
 const store = configureStore();
-const NavigationWithRouter = withRouter(Navigation);
 
 export const App = () => {
   return (
     <Provider store={store}>
-      <NavigationWithRouter />
-      <Routes />
+      <Routes>
+        <Navigation />
+      </Routes>
     </Provider>
   );
 };
--- libs/ui/src/lib/organisms/Navigation/Navigation.tsx
+++ libs/ui/src/lib/organisms/Navigation/Navigation.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 export const Navigation = () => {
   return (
-    <Navbar bg="primary" variant="dark">
-      <Navbar.Brand href="/">Home</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="/contact">Contact Us</Nav.Link>
-          <Nav.Link href="/register">Sign up</Nav.Link>
-          <Nav.Link href="/login">Sign in</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <nav className="navbar navbar-expand-sm bg-primary navbar-dark">
+      <ul className="navbar-nav">
+        <li className="navbar-brand">
+          <NavLink exact to="/" className="nav-link">
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-link">
+          <NavLink to="/about" className="nav-link">
+            About
+          </NavLink>
+        </li>
+        <li className="nav-link">
+          <NavLink to="/contact" className="nav-link">
+            Contact Us
+          </NavLink>
+        </li>
+        <li className="nav-link">
+          <NavLink to="/register" className="nav-link">
+            Sign Up
+          </NavLink>
+        </li>
+        <li className="nav-link">
+          <NavLink to="/login" className="nav-link">
+            Sign In
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
