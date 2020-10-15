b38250f4b26753322500b436295406234fad044e
byalcin <mbatuhanyalcin006@hotmail.com>
2020-10-15 15:02:39 +0300

    loginGoogle

--- apps/react/src/app/pages/Login.tsx
+++ apps/react/src/app/pages/Login.tsx
@@ -6,6 +6,8 @@ import { useDispatch } from 'react-redux';
 import { loginAsync } from '@internship/store/authentication';
 import { useTemporary } from '@internship/shared/hooks';
 import { Captcha } from '@internship/ui';
+import GoogleLogin from 'react-google-login';
+import { api } from '@internship/shared/api';
 
 const StyledApp = styled.div`
   font-family: sans-serif;
@@ -41,12 +43,29 @@ export const Login = (context) => {
   const dispatch = useDispatch();
   const { isCaptchaRequired } = useTemporary();
 
+  const responseGoogle = (response) => {
+    console.log(response);
+    localStorage.setItem('cloud_users', JSON.stringify(response));
+    api.auth.loginGoogle(response);
+  }
+
   const onSubmit = (values) => {
     dispatch(loginAsync.request(values));
   };
 
+   const API_BASE_URL = 'http://localhost:8080';
+
+   const ACCESS_TOKEN = 'accessToken';
+
+   const OAUTH2_REDIRECT_URI = 'http://localhost:4200/oauth2/redirect'
+
+   const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
+
+
   return (
-    <StyledApp>
+
+  <StyledApp>
+
       <form onSubmit={handleSubmit(onSubmit)}>
         <H4>Enter your information to log into your account.</H4>
         <Container>
@@ -78,6 +97,13 @@ export const Login = (context) => {
             <a href="/register">Sign Up</a>
           </StyledRow>
           <Button type="submit">Submit</Button>
+          <GoogleLogin
+            clientId="23500386943-9hrfde5uu9qvugpa1bq3cacdsqbbdu0o.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
         </Container>
       </form>
     </StyledApp>
--- libs/shared/api/src/lib/api/Auth/auth-resource.ts
+++ libs/shared/api/src/lib/api/Auth/auth-resource.ts
@@ -5,5 +5,6 @@ export class AuthResource {
 
   // TODO change any types according to request/response model
   login = (data: any): Promise<any> => this.axios.post('auth/signin', data, this.axiosRequestConfig).then((r) => r.data);
+  loginGoogle = (data: any): Promise<any> => this.axios.post('/oauth2/authorize/google?redirect_uri=http://localhost:4200/signin', data, this.axiosRequestConfig).then((r) => r.data);
   register = (data: any): Promise<any> => this.axios.post('auth/sign-up', data, this.axiosRequestConfig).then((r) => r.data);
 }
