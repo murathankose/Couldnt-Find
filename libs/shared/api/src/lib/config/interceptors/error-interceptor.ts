f16a16c9f91111fcfd83ecdf5169bf7ba184fb45
MArifSahin <mehmetarifsahin@etu.edu.tr>
2020-10-23 16:53:39 +0300

    Add navbar toggler icon.
    Navbar is collapsed on small screen sizes and navbar toggler icon is appeared.

--- libs/shared/api/src/lib/config/interceptors/error-interceptor.ts
+++ libs/shared/api/src/lib/config/interceptors/error-interceptor.ts
@@ -3,8 +3,10 @@ import { AxiosError } from 'axios';
 export const errorinterceptor = (error: AxiosError) => {
   switch (error.response && error.response.status) {
     case 401: {
-      window['UGLY_STORE'].dispatch({ type: '@temp/ERROR_REQUIRED', payload: 'Kullanıcı adı veya şifre yanlış.' });
-      break;
+      if(error.response.data?.error.toString() === 'JWT cannot be empty'){
+        window['UGLY_STORE'].dispatch({ type: '@temp/ERROR_REQUIRED', payload: 'Kullanıcı adı veya şifre yanlış.' });
+        break;
+      }
     }
   }
   throw error;
--- libs/ui/src/lib/organisms/Navigation/Navigation.tsx
+++ libs/ui/src/lib/organisms/Navigation/Navigation.tsx
@@ -12,6 +12,8 @@ export const Navigation = () => {
   const [show, setShow] = useState(false);
   const dispatch = useDispatch();
   const history = useHistory();
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
   const handleClose = () => {
     setShow(false);
@@ -26,56 +28,67 @@ export const Navigation = () => {
   };
   return (
     <nav className="navbar navbar-expand-sm bg-primary navbar-dark">
-      <ul className="navbar-nav mr-auto">
-        <li className="navbar-brand">
-          <NavLink exact to="/" className="nav-link">
-            Home
-          </NavLink>
-        </li>
-        <li className="nav-link">
-          <NavLink to="/about" className="nav-link">
-            About
-          </NavLink>
-        </li>
-        <li className="nav-link">
-          <NavLink to="/contact" className="nav-link">
-            Contact Us
-          </NavLink>
-        </li>
-        {isAuthenticated ? (
-          <NavDropdown className="nav-link" title={<FaUserAlt />} id="basic-nav-dropdown">
-            <li className="nav-link">
-              <NavLink to="/profile" className="nav-link active bg-primary w-50 rounded">
-                Profile
+      <div className="container">
+        <button className="custom-toggler navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbar" aria-controls="navbar"
+                aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation"
+                onClick={handleNavCollapse}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbar">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-brand">
+              <NavLink exact to="/" className="nav-link">
+                Home
               </NavLink>
             </li>
             <li className="nav-link">
-              <NavLink type="button" to="/profile" className="nav-link active bg-primary w-50  rounded" onClick={handleOpen}>
-                Logout
+              <NavLink to="/about" className="nav-link">
+                About
               </NavLink>
-              <Popup show={show} onHide={handleClose}>
-                Sistemden Çıkıyorsunuz Emin misiniz?
-                <PopupButton variant="secondary" onClick={handleClose}>
-                  HAYIR
-                </PopupButton>
-                <PopupButton variant="primary" onClick={handleShow}>
-                  EVET
-                </PopupButton>
-              </Popup>
             </li>
-          </NavDropdown>
-        ) : (
-          <NavDropdown className="nav-link" title="Account" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#">
-              <NavLink to="/register">Sign Up</NavLink>
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#">
-              <NavLink to="/login">Sign In</NavLink>
-            </NavDropdown.Item>
-          </NavDropdown>
-        )}
-      </ul>
-      <Search />
+            <li className="nav-link">
+              <NavLink to="/contact" className="nav-link">
+                Contact Us
+              </NavLink>
+            </li>
+            {isAuthenticated ? (
+              <NavDropdown className="nav-link" title={<FaUserAlt />} id="basic-nav-dropdown">
+                <li className="nav-link">
+                  <NavLink to="/profile" className="nav-link active bg-primary w-50 rounded">
+                    Profile
+                  </NavLink>
+                </li>
+                <li className="nav-link">
+                  <NavLink type="button" to="/profile" className="nav-link active bg-primary w-50  rounded" onClick={handleOpen}>
+                    Logout
+                  </NavLink>
+                  <Popup show={show} onHide={handleClose}>
+                    Sistemden Çıkıyorsunuz Emin misiniz?
+                    <PopupButton variant="secondary" onClick={handleClose}>
+                      HAYIR
+                    </PopupButton>
+                    <PopupButton variant="primary" onClick={handleShow}>
+                      EVET
+                    </PopupButton>
+                  </Popup>
+                </li>
+              </NavDropdown>
+            ) : (
+              <NavDropdown className="nav-link" title="Account" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#">
+                  <NavLink to="/register">Sign Up</NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#">
+                  <NavLink to="/login">Sign In</NavLink>
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
+            <Search />
+          </ul>
+
+        </div>
+      </div>
     </nav>
   );
 };
