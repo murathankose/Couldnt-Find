e221f6fefe2bf874a30717ce4636b514d4246f74
Murathan Köse <murathankose@icloud.com>
2020-10-23 16:51:03 +0300

    Added some features

--- apps/react/src/app/pages/profilePageComponents/EditProfile.tsx
+++ apps/react/src/app/pages/profilePageComponents/EditProfile.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { UserForm } from '@internship/ui';
 
-export const EditProfile = ({ ...props }) => {
+export const EditProfile = ({ setInEditMode, setEditUserInfo, ...props }) => {
   return (
     <Container fluid>
       <Row>
         <h3>Edit Profile</h3>
       </Row>
-      <UserForm />
+      <UserForm setEditUserInfo={setEditUserInfo} setInEditMode={setInEditMode} />
     </Container>
   );
 };
