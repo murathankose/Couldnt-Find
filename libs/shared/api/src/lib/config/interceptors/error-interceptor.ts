a5e922207891fbcbc28dff9be34f3d0ea0c4a644
MArifSahin <mehmetarifsahin@etu.edu.tr>
2020-11-02 09:57:09 +0300

    Add account not activated error message.

--- libs/shared/api/src/lib/config/interceptors/error-interceptor.ts
+++ libs/shared/api/src/lib/config/interceptors/error-interceptor.ts
@@ -4,6 +4,7 @@ import { getRefreshToken, setAccessToken } from '@internship/shared/utils';
 const err = {
   'auth/signin': {
     '401': 'Kullanıcı adı veya şifre yanlış',
+    '401-2': 'Account not activated. Please activate your account!',
     '429': 'Kullanıcı adı veya şifre yanlış'
   },
   'auth/sign-up': {
@@ -27,6 +28,8 @@ const err = {
 };
 export const errorInterceptor = (error: AxiosError, axios: AxiosInstance = axiosStatic) => {
   let errorMessage = err[error.config.url][error.response?.status];
+  console.log(error.response?.data.error.toString());
+  console.log(error.config.url);
   if (error.config.url === 'user/change-password' && error.response?.status === 400) {
     if (error.response?.data.error.toString() === 'Your old password is not correct') {
       errorMessage = err[error.config.url]['400-1'];
@@ -34,12 +37,19 @@ export const errorInterceptor = (error: AxiosError, axios: AxiosInstance = axios
       errorMessage = err[error.config.url]['400-2'];
     }
   }
-
-  if (error.config.url === 'auth/sign-up' && error.response?.status === 400) {
+  else if (error.config.url === 'auth/sign-up' && error.response?.status === 400) {
     if (error.response?.data.error.toString() === 'Email is already in use!') {
       errorMessage = err[error.config.url]['400-1'];
     } else {
       errorMessage = err[error.config.url]['400-2'];
+    }
+  }
+  else if (error.config.url === 'auth/signin' && error.response?.status === 401) {
+    console.log(error.response?.data.error.toString());
+    if (error.response?.data.error.toString() === 'Account not activated. Please activate your account!') {
+      errorMessage = err[error.config.url]['401-2'];
+    } else {
+      errorMessage = err[error.config.url]['401'];
     }
   }
   window['UGLY_STORE'].dispatch({ type: '@temp/ERROR_REQUIRED', payload: errorMessage });
