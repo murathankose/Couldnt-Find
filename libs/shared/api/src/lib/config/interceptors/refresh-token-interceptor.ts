5586e9e8a48bac910caab5028ebe8934bd1956c4
MArifSahin <mehmetarifsahin@etu.edu.tr>
2020-11-02 16:43:27 +0300

    Refactor refresh-token-interceptor.ts

--- libs/shared/api/src/lib/config/interceptors/refresh-token-interceptor.ts
+++ libs/shared/api/src/lib/config/interceptors/refresh-token-interceptor.ts
@@ -1,6 +1,7 @@
 import axiosStatic, { AxiosError, AxiosInstance } from 'axios';
-import { getRefreshToken, removeAccessToken, setAccessToken } from '@internship/shared/utils';
-
+import { getRefreshToken, removeAccessToken, removeRefreshToken, setAccessToken } from '@internship/shared/utils';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { updateLogout } from '@internship/store/authentication';
 export const refreshTokenInterceptor = (error: AxiosError, axios: AxiosInstance = axiosStatic) => {
   if (error.response.status === 401 && error.response.data?.error.toString() === 'JWT Expired.') {
     return new Promise((resolve) => {
@@ -15,7 +16,13 @@ export const refreshTokenInterceptor = (error: AxiosError, axios: AxiosInstance 
             const res = axios.request(error.config);
             resolve(res);
           }
+        })
+        .catch(error => {
+          window['UGLY_STORE'].dispatch({ type: '@temp/ERROR_REQUIRED', payload:"Your session has expired. Please login again!" });
+          window['UGLY_STORE'].dispatch(updateLogout());
         });
+
+
     });
   }
   throw error;
