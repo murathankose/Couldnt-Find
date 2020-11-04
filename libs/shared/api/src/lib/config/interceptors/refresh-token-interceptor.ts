70c3817d52e22afb1119d05cd0e5ec4c341743e9
Evasionn <asalyazilim@gmail.com>
2020-11-04 23:03:21 +0300

    fix circular dependency

--- libs/shared/api/src/lib/config/interceptors/refresh-token-interceptor.ts
+++ libs/shared/api/src/lib/config/interceptors/refresh-token-interceptor.ts
@@ -1,8 +1,6 @@
-import axiosStatic, { AxiosError, AxiosInstance } from 'axios';
 import { getRefreshToken, setAccessToken } from '@internship/shared/utils';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { updateLogout } from "../../../../../../../libs/store/authentication/src/lib/actions";
-//TODO circular dependency problem may arise when relative path is given.
+import axiosStatic, { AxiosError, AxiosInstance } from 'axios';
+
 export const refreshTokenInterceptor = (error: AxiosError, axios: AxiosInstance = axiosStatic) => {
   if (error.response.status === 401 && error.response.data?.error.toString() === 'JWT Expired.') {
     return new Promise((resolve) => {
@@ -23,7 +21,7 @@ export const refreshTokenInterceptor = (error: AxiosError, axios: AxiosInstance 
             type: '@temp/ERROR_REQUIRED',
             payload: 'Your session has expired. Please login again!'
           });
-          window['UGLY_STORE'].dispatch(updateLogout());
+          window['UGLY_STORE'].dispatch({type: '@Authentication/UPDATE_LOGOUT'});
         });
 
     });
