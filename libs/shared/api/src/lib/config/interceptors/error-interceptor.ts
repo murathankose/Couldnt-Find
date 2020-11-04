b21d83067448f18da7610e616b27c1f1a65dbaf0
byalcin <mbatuhanyalcin006@hotmail.com>
2020-11-04 16:48:00 +0300

    error-interceptor refactoring.

--- libs/shared/api/src/lib/config/interceptors/error-interceptor.ts
+++ libs/shared/api/src/lib/config/interceptors/error-interceptor.ts
@@ -3,22 +3,21 @@ const err = {
   'auth/signin': {
     '401': 'Kullanıcı adı veya şifre yanlış',
     '401-2': 'Account not activated. Please activate your account!',
-    '429': 'Kullanıcı adı veya şifre yanlış',
   },
   'auth/sign-up': {
-    '400-1': 'Email is already in use',
-    '400-2': 'Username is already taken',
+    '102': 'Email is already in use',
+    '101': 'Username is already taken',
   },
   'user/change-password': {
-    '400-1': 'Old password is incorrect',
-    '400-2': 'Password fields does not match',
+    '108': 'Old password is incorrect',
+    '109': 'Password fields does not match',
   },
   'user/edit': {
     '400': 'Email is already in use!',
     '500': 'Phone Number Error',
   },
   'auth/forgot-password': {
-    '400': 'No such user',
+    '105': 'There is no registered user on this email.',
   },
   'user/create-new-password': {
     '400': 'Something is wrong with that token!',
@@ -28,27 +27,29 @@ const err = {
   },
 };
 export const errorInterceptor = (error: AxiosError) => {
+  console.log(error.response.data.message);
+  console.log(error.config.url);
   let errorMessage = null;
-  if (error.config.url === 'user/change-password' && error.response?.status === 400) {
-    if (error.response?.data.error.toString() === 'Your old password is not correct') {
-      errorMessage = err[error.config.url]['400-1'];
-    } else {
-      errorMessage = err[error.config.url]['400-2'];
-    }
-  } else if (error.config.url === 'auth/sign-up' && error.response?.status === 400) {
-    if (error.response?.data.error.toString() === 'Email is already in use!') {
-      errorMessage = err[error.config.url]['400-1'];
-    } else {
-      errorMessage = err[error.config.url]['400-2'];
-    }
+  if (error.response?.data.message.toString() === '108') {
+    errorMessage = err[error.config.url]['108'];
+  } else if (error.response?.data.message.toString() === '109') {
+    errorMessage = err[error.config.url]['109'];
+  } else if (error.response?.data.message.toString() === '101') {
+    errorMessage = err[error.config.url]['101'];
+  } else if (error.response?.data.message.toString() === '102') {
+    errorMessage = err[error.config.url]['102'];
+  } else if (error.response?.data.message.toString() === '105') {
+    errorMessage = err[error.config.url]['105'];
+  } else if (error.response?.data.message.toString() === '108') {
+    errorMessage = err[error.config.url]['108'];
+  } else if (error.response?.data.message.toString() === 'User with given email could not found') {
+    errorMessage = err['auth/send-email']['400'];
   } else if (error.config.url === 'auth/signin' && error.response?.status === 401) {
     if (error.response?.data.error.toString() === 'Account not activated. Please activate your account!') {
       errorMessage = err[error.config.url]['401-2'];
     } else {
       errorMessage = err[error.config.url]['401'];
     }
-  } else if (error.config.url.startsWith('auth/send-email') && error.response?.status === 400) {
-    errorMessage = err['auth/send-email']['400'];
   } else if (error.response?.data.error.toString() === 'Invalid Login details' && error.response?.status === 401) {
     window['UGLY_STORE'].dispatch({ type: '@Authentication/UPDATE_LOGOUT' });
     errorMessage = null;
